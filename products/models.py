b3bd180f7039d53c22bd2351d53fa9698aaa5032
ROOZBEH <MR.FAKE@gmail.com>
2022-12-18 12:39:19 +0330

    make product models and migrate it

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
+from audioop import reverse
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('product name'))
+    description = models.TextField(_('product description'))
+    price = models.PositiveIntegerField(_('product price'), default=0)
+    
+    datetime_created = models.DateTimeField(_('datetime_created'), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_('datetime_modified'), auto_now=True)
+    
+    is_active = models.BooleanField(_('is_active'), default=True)
+
+    def __str__(self):
+        return f'product name:{self.name} product is_active : {self.is_active}'
+    
+    # def get_absolute_url(self):
+    #     return reverse('product_details', args=[self.product_id])
