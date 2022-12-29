989eeeb77c25a30aa57fddff699de2ec783f5817
ROOZBEH <MR.FAKE@gmail.com>
2022-12-29 15:57:42 +0330

    make templatetags for convert english number to persian

---
+++ translate/templatetags/translate_number.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library
+
+@register.filter
+def persian_numbers(number):
+    
+    english_numbers = "0123456789"
+    
+    persian_numbers = "۰۱۲۳۴۵۶۷۸۹"
+    
+    number = str(number)
+    
+    convert_e_to_p_numbers = number.maketrans(english_numbers, persian_numbers)
+    
+    return number.translate(convert_e_to_p_numbers)
